935e3c50065710922cf87ec974f6b89a18c1fa04
Sergey9911 <kormilin.sergey@gmail.com>
2024-10-10 18:59:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+first= 13
+second= 118
+third = 13
+if second == second == third:
+    print(3)
+if first == third or first != second == third:
+    print(2)
+if first != second != third:
+    print(0)
+
+
+
